2d86821e16f5f76b08f5c666505d82dff6f4b4de
panozzaj <panozzaj@gmail.com>
2009-04-19 18:03:57 -0400

    20090201 version (1.02)

--- generator.rb
+++ generator.rb
@@ -1,8 +1,10 @@
 output = File.open("autocorrect.vim", "w")
 File.open("autocorrect.dat").each do |line|
-  parts = line.split("->")
-  output.puts "iabbrev #{parts[0]} #{parts[1]}"
-  if parts[0].capitalize != parts[0]
-    output.puts "iabbrev #{parts[0].capitalize} #{parts[1].capitalize}"
+  parts = line.chomp.split("->") # we don't want the line ending
+  output.puts "ia #{parts[0]} #{parts[1]}"
+  # if the words are already capitalized or the correction is capitalization, skip
+  capitalized = parts[0].capitalize
+  unless capitalized == parts[0] || capitalized == parts[1]
+    output.puts "ia #{capitalized} #{parts[1].capitalize}"
   end
 end
