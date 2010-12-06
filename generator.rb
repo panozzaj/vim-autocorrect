5ff1ce476c9514414398e298447df8294c9d1460
Anthony Panozzo <panozzaj@gmail.com>
2010-12-05 23:04:05 -0500

    Add basic first two letter correction.

--- generator.rb
+++ generator.rb
@@ -1,16 +1,75 @@
-# Ruby's capitalize doesn't quite do what we want for wifi->Wi-Fi,
-# since "Wi-Fi".capitalize => "Wi-fi"
-def my_capitalize(s)
-  s[0..0].upcase + s[1..-1]
-end
+# Seems like doing some actual unit tests would be advisable,
+# but leaving for now. The examples are all there, should be easy to extract.
+class Generator
+  def generate
+    output = File.open("autocorrect.vim", "w")
+    File.open("autocorrect.dat").each do |line|
+      parts = line.chomp.split("->") # we don't want the line ending
+      wrong = parts[0]
+      right = parts[1]
 
-output = File.open("autocorrect.vim", "w")
-File.open("autocorrect.dat").each do |line|
-  parts = line.chomp.split("->") # we don't want the line ending
-  output.puts "ia #{parts[0]} #{parts[1]}"
-  # if the words are already capitalized or the correction is capitalization, skip
-  capitalized = my_capitalize(parts[0])
-  unless capitalized == parts[0] || capitalized == parts[1]
-    output.puts "ia #{capitalized} #{my_capitalize(parts[1])}"
+      write output, basic_correction(wrong, right)
+      write output, capitalized_correction(wrong, right)
+      write output, capitalization_correction(wrong, right)
+      write output, first_two_letters_correction(wrong, right)
+    end
+  end
+
+  def write(output, s)
+    output.puts s unless s.nil?
+  end
+
+  # Ruby's capitalize doesn't quite do what we want for wifi->Wi-Fi,
+  # since "Wi-Fi".capitalize => "Wi-fi"
+  def capitalize(s)
+    s[0..0].upcase + s[1..-1]
+  end
+
+  def capitalize_first_two(s)
+    s[0..1].upcase + s[2..-1]
+  end
+
+  def capitalized?(s)
+    s == capitalize(s)
+  end
+
+  # ex: teh->the
+  #  =  teh->teh
+  # ex: Bernouilli->Bernoulli
+  #  =  Bernouilli->Bernoulli
+  def basic_correction(wrong, right)
+    "ia #{wrong} #{right}"
+  end
+
+  # ex: teh -> the
+  #  =  Teh -> The
+  def capitalized_correction(wrong, right)
+    capitalized = capitalize wrong
+    # if the words are already capitalized or the correction _is_ capitalization, skip
+    unless capitalized == wrong || capitalized == right
+      "ia #{capitalized} #{capitalize right}"
+    end
+  end
+
+  # ex: Bernouilli->Bernoulli
+  #  =  bernouilli->Bernoulli
+  def capitalization_correction(wrong, right)
+    if capitalized?(right) && capitalized?(wrong)
+      "ia #{wrong.downcase} #{right}"
+    end
+  end
+
+  # ex: teh->the
+  #  =  TEh->The
+  # ex: Bernouilli->Bernoulli
+  #  =  BErnouilli->Bernoulli
+  def first_two_letters_correction(wrong, right)
+    if wrong.length > 2 && right.length > 2
+        wrong != capitalize_first_two(wrong) &&
+        right != capitalize_first_two(right)
+      "ia #{capitalize_first_two(wrong)} #{capitalize right}"
+    end
   end
 end
+
+Generator.new.generate
